da37f2549e64e435a84fda15383cc0177d8cf953
Taehoya <cth6809@naver.com>
2023-07-31 17:17:56 -0500

    chore: add dummy test

---
+++ src/domain/user/user.service.spect.ts
@@ -0,0 +1,8 @@
+//TODO: remove test once user service is implemented
+describe('dummy test ', () => {
+  it('dummy test', () => {
+    const expected = 2;
+    const result = 1 + 1;
+    expect(result).toEqual(expected);
+  });
+});
