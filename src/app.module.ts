988897097928a06433ccaa70ab84da6fd10e0409
Taehoya <cth6809@naver.com>
2023-08-07 12:08:00 -0500

    KB-4 move healthcheck to app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HealthController } from './health/health.controller';
-import { TerminusModule } from '@nestjs/terminus';
-import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TerminusModule, HttpModule],
-  controllers: [AppController, HealthController],
+  imports: [],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
