89a84b170e3e7c6151cc454882548193cf62665b
Taehoya <cth6809@naver.com>
2023-08-07 12:42:37 -0500

    correct test description

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -3,7 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './app.module';
 
-describe('PlaygroundController', () => {
+describe('AppController', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
