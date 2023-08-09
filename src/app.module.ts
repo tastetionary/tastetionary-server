35ffa130443a4d02d932b072a705f77d777fc830
matthew <matthew@cupist.com>
2023-08-09 18:25:20 +0900

    [feat] delete app service
    we don need to use that

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppService } from '@src/app.service';
 import { PlaygroundModule } from '@src/domain/playground/playground.module';
 import { AppController } from '@src/app.controller';
+import { ConfigurationModule } from './configuration/configuration.module';
 @Module({
   controllers: [AppController],
   imports: [
@@ -12,7 +12,7 @@ import { AppController } from '@src/app.controller';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    ConfigurationModule,
   ],
-  providers: [AppService],
 })
 export class AppModule {}
