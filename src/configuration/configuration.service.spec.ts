5004ae5668e59508582a8b957ea7ecb255b78cc3
matthew <matthew@cupist.com>
2023-08-09 18:26:39 +0900

    [chore] modify name

--- src/configuration/configuration.service.spec.ts
+++ src/configuration/configuration.service.spec.ts
@@ -1,27 +1,27 @@
 import { TestingModule } from '@nestjs/testing';
 import { appServiceFixture } from '@root/jest.setup';
-import { ConfigurationService } from '@src/configuration/configuration.service';
+import { ConfigurationService as cfgService } from '@src/configuration/configuration.service';
 
 describe('configuration service', () => {
   beforeEach(async () => {});
 
-  describe('fixture', () => {
+  describe('get server config', () => {
     it.skip('reason: env is not cleared so this test affect other test, fixture with dev env should return expected', async () => {
       const module = (await appServiceFixture(
-        [ConfigurationService],
+        [cfgService],
         'dev',
       )) as TestingModule;
-      const service = module.get<ConfigurationService>(ConfigurationService);
+      const service = module.get<cfgService>(cfgService);
       const res = service.getServerConfig();
       expect(res.env).toEqual('DEV');
     });
 
     it('fixture with test env should return expected', async () => {
       const module = (await appServiceFixture(
-        [ConfigurationService],
+        [cfgService],
         'test',
       )) as TestingModule;
-      const service = module.get<ConfigurationService>(ConfigurationService);
+      const service = module.get<cfgService>(cfgService);
       const res = service.getServerConfig();
       expect(res.env).toEqual('TEST');
     });
