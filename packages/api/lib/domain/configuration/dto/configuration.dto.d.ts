a84d979fa0735697b8c3de492aed66ea519c7c47
matthew <homekeeper89@gmail.com>
2023-08-20 13:09:08 +0900

    [chore] modify readme

---
+++ packages/api/lib/domain/configuration/dto/configuration.dto.d.ts
@@ -0,0 +1,7 @@
+export interface ServerConfig {
+    ENV: string;
+}
+export interface ServerMetaData {
+    serverTime: string;
+    version: string;
+}
---
+++ packages/api/lib/domain/playground/dto/playground.dto.d.ts
@@ -0,0 +1,14 @@
+export interface PlaygroundSampleDto {
+    /**
+     * Title of the article.
+     */
+    title: string;
+    /**
+     * Content body.
+     */
+    body: string;
+    /**
+     * Password of the article.
+     */
+    password: string;
+}
