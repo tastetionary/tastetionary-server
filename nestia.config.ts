a8893e3a377d0ee343639a3f4652adec75809983
matthew <homekeeper89@gmail.com>
2023-08-20 10:51:44 +0900

    [feat] impl nestia and set config

--- nestia.config.ts
+++ nestia.config.ts
@@ -1,7 +1,25 @@
 import { INestiaConfig } from '@nestia/sdk';
+
 const config: INestiaConfig = {
   input: 'src/**/*.controller.ts',
-  output: './taste_dict',
+  output: 'src/api',
   distribute: 'packages/api',
+  e2e: 'test',
+  swagger: {
+    output: 'dist/swagger.json',
+    security: {
+      bearer: {
+        type: 'apiKey',
+        name: 'Authorization',
+        in: 'header',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local Server',
+      },
+    ],
+  },
 };
 export default config;
