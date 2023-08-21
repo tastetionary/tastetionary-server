ca82b7d5b883df1c1b94346f407fd6c8dee8b38e
Taehoya <cth6809@naver.com>
2023-08-21 12:32:01 -0500

    chore: remove pagination response and unnecessary attributes

--- src/common/dto/base.dto.ts
+++ src/common/dto/base.dto.ts
@@ -1,19 +1,13 @@
 export interface BaseResponseArgs {
-  code?: number;
-  message?: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data: any;
 }
 
 export class BaseResponseDto<T> {
-  code: number;
-  message: string;
   data: T;
 
   public constructor(args: BaseResponseArgs) {
-    const { code, message = 'SUCCESS', data } = args;
-    this.code = code;
-    this.message = message;
+    const { data } = args;
     this.data = data;
   }
 }
