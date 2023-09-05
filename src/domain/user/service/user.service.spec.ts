5aeea26f3b675215326b69a866727dc2792705ef
matthew <homekeeper89@gmail.com>
2023-09-05 23:51:36 +0900

    [feat] apply register in user service

--- src/domain/user/service/user.service.spec.ts
+++ src/domain/user/service/user.service.spec.ts
@@ -4,6 +4,9 @@ import { PrismaService } from '@common/database/prisma.service';
 import { ConfigurationService } from '@domain/configuration/configuration.service';
 import { UserRepository } from '@domain/user/repository/user.repository';
 import { UserService } from '@domain/user/service/user.service';
+import { AccountRepository } from '@domain/user/repository/account.repository';
+import { RegisterAccountDto } from '@domain/user/dto/user.dto';
+import { AccountCategory } from '@domain/user/user.enum';
 
 describe('user service', () => {
   let service: UserService;
@@ -11,21 +14,35 @@ describe('user service', () => {
   beforeAll(async () => {
     const module = (await appModuleFixture(
       [],
-      [UserService, ConfigurationService, PrismaService, UserRepository],
+      [
+        UserService,
+        ConfigurationService,
+        PrismaService,
+        UserRepository,
+        AccountRepository,
+      ],
     )) as TestingModule;
     service = module.get(UserService);
     prisma = module.get(PrismaService);
   });
 
   beforeEach(async () => {
-    await truncateTables(prisma, ['users']);
+    await truncateTables(prisma, ['users', 'accounts']);
   });
 
-  it('should create user', async () => {
-    await service.createUsers({
-      nickname: 'test',
-      state: 'test',
-      property: {},
-    });
+  it('should create user and account', async () => {
+    const dto: RegisterAccountDto = {
+      identification: 'test',
+      password: 'pwd',
+      category: AccountCategory.EMAIL,
+      agreement: [
+        {
+          category: 'PERSONAL',
+          is_agree: true,
+        },
+      ],
+    };
+    const user = await service.registerEndUser(dto);
+    expect(user).not.toBeNull();
   });
 });
--- src/domain/user/service/user.service.ts
+++ src/domain/user/service/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from '@domain/user/repository/user.repository';
+import { AccountRepository } from '@domain/user/repository/account.repository';
+import { EndUser } from '@domain/user/core/end-user';
+import { Account } from '@domain/user/core/account';
+import { RegisterAccountDto } from '@domain/user/dto/user.dto';
 
 @Injectable()
 export class UserService {
-  constructor(private userRepo: UserRepository) {}
+  constructor(
+    private userRepo: UserRepository,
+    private accountRepo: AccountRepository,
+  ) {}
 
-  async createUsers() {}
+  async registerEndUser(dto: RegisterAccountDto) {
+    const endUser = new EndUser(this.userRepo);
+    const user = await endUser.register();
+
+    const account = new Account(this.accountRepo, dto);
+    await account.register(user.id);
+    return user;
+  }
 }
