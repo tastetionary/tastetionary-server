0b3aa2bbe666af381ae1d1ba8ae7ac28481caeaf
Taehoya <cth6809@naver.com>
2023-10-03 08:55:30 -0500

    [chore] add food dto

---
+++ src/domain/food/dto/food.dto.ts
@@ -0,0 +1,26 @@
+import { FoodCategory, FoodKeyword } from '@domain/food/food.enum';
+
+export interface FoodOption {
+  /**
+   * food category,
+   * example: "한식"
+   * @type FoodCategory[]
+   */
+  categories: FoodCategory[];
+
+  /**
+   * food keywords
+   * example: "깨끗해요"
+   * @type FoodKeyword[]
+   */
+  keywords: FoodKeyword[];
+}
+
+export interface FoodRecommend {
+  /**
+   * recommended food
+   * example: "김치찌개"
+   * @type string
+   */
+  name: string;
+}
