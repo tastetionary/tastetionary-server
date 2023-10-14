d749de9e69574b591fe5e421a5af2380d8675e0b
matthew <homekeeper89@gmail.com>
2023-10-14 20:42:05 +0900

    [chore] add temp key to use default value
    it will reveted

--- src/domain/configuration/configuration.service.ts
+++ src/domain/configuration/configuration.service.ts
@@ -37,17 +37,14 @@ export class ConfigurationService {
     return {
       apiKey: this.configService.get<string>('MAIL_GUN_API_KEY', ''),
       domain: this.configService.get<string>(
-        'MAIL_GUN_DOMAIN',
+        'temp',
         'sandbox3337a931a909471584fd050d91a3d035.mailgun.org',
       ),
       fromEmail: this.configService.get<string>(
-        'MAIL_GUN_FROM_EMAIL',
+        'temp',
         'no-reply@tastetionary.com',
       ),
-      fromTitle: this.configService.get<string>(
-        'MAIL_GUN_FROM_TITLE',
-        '맛셔너리팀',
-      ),
+      fromTitle: this.configService.get<string>('temp', '맛셔너리팀'),
     };
   }
 }
