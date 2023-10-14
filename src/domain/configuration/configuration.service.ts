09a9c0ca8da634538d7e57a829232c5d71d462e7
matthew <homekeeper89@gmail.com>
2023-10-14 20:41:38 +0900

    [chore] modify env

--- src/domain/configuration/configuration.service.ts
+++ src/domain/configuration/configuration.service.ts
@@ -35,17 +35,17 @@ export class ConfigurationService {
 
   getMailGunConfig() {
     return {
-      apiKey: this.configService.get<string>('mailGunApiKey', ''),
+      apiKey: this.configService.get<string>('MAIL_GUN_API_KEY', ''),
       domain: this.configService.get<string>(
-        'mailGunDomain',
+        'MAIL_GUN_DOMAIN',
         'sandbox3337a931a909471584fd050d91a3d035.mailgun.org',
       ),
       fromEmail: this.configService.get<string>(
-        'mailGunFromEmail',
+        'MAIL_GUN_FROM_EMAIL',
         'no-reply@tastetionary.com',
       ),
       fromTitle: this.configService.get<string>(
-        'mailGunFromTitle',
+        'MAIL_GUN_FROM_TITLE',
         '맛셔너리팀',
       ),
     };
