13189837dabaac58949f7b2fdf7d32b9ff043ffb
matthew <homekeeper89@gmail.com>
2023-10-22 14:31:45 +0900

    [chore] delete unnecessary line

--- src/common/auth/auth.guard.ts
+++ src/common/auth/auth.guard.ts
@@ -39,7 +39,6 @@ export class AuthGuard implements CanActivate {
       // 💡 We're assigning the payload to the request object here
       // so that we can access it in our route handlers
 
-      console.log(payload);
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException(
