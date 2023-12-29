9870771e82c2f8dab8774dd6e2021736f7bd6700
matthew <homekeeper89@gmail.com>
2023-12-29 18:02:39 +0900

    [feat] impl withdraw

--- src/domain/user/facade/user.facade.spec.ts
+++ src/domain/user/facade/user.facade.spec.ts
@@ -1,9 +1,60 @@
 import { AccountCategory } from '@domain/account/account.enum';
 import { RegisterUserDTO } from '@domain/user/dto/user.dto';
-import { AgreementCategory, AreaCategory } from '@domain/user/user.enum';
-import { getProfile, registerProfile } from '@domain/user/facade/user.facade';
+import {
+  AccountCancellationTypeEnum,
+  AgreementCategory,
+  AreaCategory,
+  UserState,
+} from '@domain/user/user.enum';
+import {
+  getProfile,
+  registerProfile,
+  withdrawFrom,
+} from '@domain/user/facade/user.facade';
 
 describe('user facade', () => {
+  it('withdrawFrom should update state ', async () => {
+    const dto: RegisterUserDTO = {
+      userProperty: {},
+      areas: [
+        {
+          latitude: 1,
+          longitude: 1,
+          category: AreaCategory.ACTIVITY_AREA,
+          address: 'test',
+        },
+        {
+          latitude: 1,
+          longitude: 1,
+          category: AreaCategory.DINING_AREA,
+          address: 'test',
+        },
+      ],
+      account: {
+        identification: `test_${new Date().getMilliseconds()}`,
+        password: 'pwd',
+        category: AccountCategory.EMAIL,
+      },
+      agreements: [
+        {
+          category: AgreementCategory.PERSONAL_INFORMATION,
+          is_agree: true,
+        },
+      ],
+    };
+
+    const user = await registerProfile(dto);
+
+    await withdrawFrom(
+      user.id,
+      AccountCancellationTypeEnum.FOUND_SIMILAR_SERVICE,
+      'bye',
+    );
+
+    const profile = await getProfile(user.id);
+    expect(profile.user.state).toEqual(UserState.WITHDRAWAL);
+  });
+
   it('registerProfile should create profile', async () => {
     const dto: RegisterUserDTO = {
       userProperty: {},
--- src/domain/user/facade/user.facade.ts
+++ src/domain/user/facade/user.facade.ts
@@ -1,8 +1,18 @@
 import {
+  changeUserState,
   createProfile,
+  createUserOpinion,
   searchProfile,
 } from '@domain/user/service/user.service';
 import { RegisterUserDTO } from '@domain/user/dto/user.dto';
+import {
+  removeAllAccount,
+  removeAllToken,
+} from '@domain/account/service/account.service';
+import {
+  AccountCancellationTypeEnum as WithdrawalTypeEnum,
+  UserState,
+} from '@domain/user/user.enum';
 
 export async function getProfile(userId: number) {
   return searchProfile(userId);
@@ -10,4 +20,15 @@ export async function getProfile(userId: number) {
 
 export async function registerProfile(dto: RegisterUserDTO) {
   return createProfile(dto);
+}
+
+export async function withdrawFrom(
+  userId: number,
+  type: WithdrawalTypeEnum,
+  opinion?: string,
+) {
+  await createUserOpinion({ userId, category: 'withdrawal', type, opinion });
+  await changeUserState(userId, UserState.WITHDRAWAL);
+  await removeAllAccount(userId);
+  await removeAllToken(userId);
 }
