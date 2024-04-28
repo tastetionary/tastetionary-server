8e7b376ac6b27612a04b7fe877135d225e2388f7
Taehoya <cth6809@naver.com>
2024-04-27 21:35:24 -0500

    chore:add review report category

--- src/domain/restaurant/restaurant.enum.ts
+++ src/domain/restaurant/restaurant.enum.ts
@@ -61,3 +61,12 @@ export const RestaurantKeywordEmoji: Record<RestaurantKeyword, string> = {
   [RestaurantKeyword.WIDE]: '🎶',
   [RestaurantKeyword.WAITING]: '💦',
 };
+
+export enum ReviewReportCategory {
+  INAPPROPRIATE_CONTENT = '부적절한 내용',
+  SPAM = '스팸',
+  HATE_SPEECH = '혐오 발언',
+  ADVERTISEMENT = '광고',
+  FALSE_INFO = '허위 정보',
+  ETC = '기타',
+}
