4075c9a7671bea3dbe1be62101f90c3053bc1ab5
Taehoya <cth6809@naver.com>
2024-10-05 11:50:47 -0500

    feat: update restaurant price options

--- src/domain/restaurant/restaurant.enum.ts
+++ src/domain/restaurant/restaurant.enum.ts
@@ -27,11 +27,11 @@ export enum RestaurantKeyword {
 }
 
 export enum RestaurantPrice {
-  UNDER_10000 = '~10,000원',
-  UNDER_11000 = '~11,000원',
-  UNDER_12000 = '~12,000원',
-  UNDER_13000 = '~13,000원',
-  OVER_13000 = '13,000원~',
+  UNDER_10000 = '10,000원 미만',
+  UNDER_13000 = '10,000원 이상 ~ 13,0000원 미만',
+  UNDER_16000 = '13,000원 이상 ~ 16,000원 미만',
+  UNDER_20000 = '16,000원 이상 ~ 20,000원 미만',
+  OVER_20000 = '20,000원 이상',
 }
 
 export const RestaurantCategoryIcons: Record<RestaurantCategory, string> = {
