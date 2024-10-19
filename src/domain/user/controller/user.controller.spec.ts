9bd76138c51ae727223439671f99b6bf5adac031
Taehoya <cth6809@naver.com>
2024-10-19 17:16:12 -0500

    feat: impl validteNickname controlelr

--- src/domain/user/controller/user.controller.spec.ts
+++ src/domain/user/controller/user.controller.spec.ts
@@ -18,6 +18,8 @@ import {
   areaEntityFactory,
   profileEntityFactory,
 } from '@root/test/factory/user.factory';
+import { CallerWrongUsageException } from '@common/exception/internal.exception';
+import { ErrorSubCategoryEnum } from '@common/exception/enum';
 
 describe('user controller', () => {
   let app: INestApplication;
@@ -180,6 +182,33 @@ describe('user controller', () => {
       });
 
     assertStatusCode(res, 200);
+  });
+
+  it('valid nickname should return success', async () => {
+    jest.spyOn(userService, 'validateNickName').mockResolvedValueOnce();
+    const nickname = 'valid nickname';
+
+    const res = await request(app.getHttpServer())
+      .get('/v1/user/nickname/validation')
+      .query({ name: nickname });
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('invalid nickname should return success', async () => {
+    jest
+      .spyOn(userService, 'validateNickName')
+      .mockRejectedValueOnce(
+        new CallerWrongUsageException(
+          ErrorSubCategoryEnum.INVALID_INPUT,
+          'invalid nickname',
+        ),
+      );
+    const nickname = 'valid nickname';
+
+    const res = await request(app.getHttpServer())
+      .get('/v1/user/nickname/validation')
+      .query({ name: nickname });
+    expect(res.statusCode).toEqual(400);
   });
 
   it('wrong input should return bad request', async () => {
