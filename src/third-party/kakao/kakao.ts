28a2be077cf48f95691f60d7e4f4a500f069c224
Taehoya <cth6809@naver.com>
2024-10-20 15:27:56 -0500

    fix: remove redirect uri

--- src/third-party/kakao/kakao.ts
+++ src/third-party/kakao/kakao.ts
@@ -34,7 +34,6 @@ async function getAccessToken(code: string) {
   const data = {
     grant_type: 'authorization_code',
     client_id: process.env.KAKAO_CLIENT_ID,
-    redirect_uri: process.env.KAKAO_REDIRECT_URI,
     code: code,
   };
 
