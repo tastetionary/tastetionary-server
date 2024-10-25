3780308d175bdf6b89b12476ee0079491cf76e42
Taehoya <cth6809@naver.com>
2024-10-24 22:35:00 -0500

    feat: impl google getAccessToken

--- src/third-party/google/google.ts
+++ src/third-party/google/google.ts
@@ -1,21 +1,48 @@
 import axios from 'axios';
 import { UnauthorizedException } from '@nestjs/common';
+import { SocialLoginInfo } from '@root/src/domain/account/dto/account.dto';
 
-export async function getGoogleUserInfo(token: string) {
+export async function getGoogleUserInfo(code: string) {
+  const accessToken = await getAccessToken(code);
   const googleUserInfoUrl = 'https://www.googleapis.com/oauth2/v3/userinfo';
 
   try {
     const userInfoResponse = await axios.get(googleUserInfoUrl, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${accessToken}`,
       },
     });
-    return userInfoResponse.data;
+
+    const res: SocialLoginInfo = {
+      email: userInfoResponse.data.email,
+      id: userInfoResponse.data.sub,
+    };
+
+    return res;
   } catch (error) {
     console.error(
       'Error fetching Google user info:',
       error.response ? error.response.data : error.message,
     );
     throw new UnauthorizedException('Failed to fetch user info from Google');
+  }
+}
+
+async function getAccessToken(code: string) {
+  try {
+    const googleTokenUrl = 'https://oauth2.googleapis.com/token';
+    const response = await axios.post(googleTokenUrl, {
+      code,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+      grant_type: 'authorization_code',
+    });
+
+    return response.data.access_token;
+  } catch (error) {
+    throw new UnauthorizedException(
+      'Failed to get access token from google: ' + error,
+    );
   }
 }
--- src/third-party/naver/naver.ts
+++ src/third-party/naver/naver.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { UnauthorizedException } from '@nestjs/common';
 
 // https://developers.naver.com/docs/login/profile/profile.md
-export async function getNaverUserInfo(token: string) {
+export async function getNaverUserInfo(code: string) {
   const naverUserInfoUrl = 'https://openapi.naver.com/v1/nid/me';
-
+  const token = await getAccessToken(code);
   try {
     const userInfoResponse = await axios.get(naverUserInfoUrl, {
       headers: {
@@ -18,5 +18,30 @@ export async function getNaverUserInfo(token: string) {
       error.response ? error.response.data : error.message,
     );
     throw new UnauthorizedException('Failed to fetch user info from Naver');
+  }
+}
+
+async function getAccessToken(code: string) {
+  const tokenUrl = 'https://nid.naver.com/oauth2.0/token';
+  const data = {
+    grant_type: 'authorization_code',
+    client_id: process.env.NAVER_CLINET_ID,
+    code: code,
+  };
+
+  try {
+    const response = await axios.post(tokenUrl, data, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+    console.log(response.data);
+    return response.data.access_token;
+  } catch (error) {
+    console.error(
+      'Error fetching access token:',
+      error.response ? error.response.data : error.message,
+    );
+    throw new UnauthorizedException('Failed to fetch access token from Kakao');
   }
 }
