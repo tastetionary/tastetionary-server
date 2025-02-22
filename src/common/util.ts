7eaeff356b566173b2d882b63626939318c7c6a1
Taehoya <cth6809@naver.com>
2025-02-22 09:35:38 -0600

    fix: update emoji regex condition

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,13 +1,14 @@
+import emojiRegex from 'emoji-regex';
+
 export function getRandomItem<T>(items: Array<T>): T {
   return items[Math.floor(Math.random() * items.length)];
 }
-
 export function detachEmoji(words: string[]) {
+  const unicodeEmojiRegex = emojiRegex();
+  const namedEmojiRegex = /:\w+:/g;
+
   return words.map((word) => {
-    return word.replace(
-      /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
-      '',
-    );
+    return word.replace(unicodeEmojiRegex, '').replace(namedEmojiRegex, '');
   });
 }
 
